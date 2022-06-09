550e6ccb98e6dd65ccfb0c926538c74dd16ed549
Sreya <challasr@umich.edu>
2022-06-09 12:30:35 -0400

    Week 1 Edits (Completed)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import Challa_Sreya_Headshot from "../assets/Challa_Sreya_Headshot.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              src={Challa_Sreya_Headshot}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Sreya Challa</div>
+            <div className="brief_description">
+              University of Michigan | Computer Science & Business Administration | Class of 2024
+            </div>
+          </div>
+        </div>
       </div>
+      
     )
   }
 }
